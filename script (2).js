c0fbe3cbccfc1b84fc04ad26c23a2a9be1f947cf
abhiramvanga766 <109586642+abhiramvanga766@users.noreply.github.com>
2022-07-20 21:55:07 +0530

    Update script (2).js

--- script (2).js
+++ script (2).js
@@ -168,7 +168,7 @@ function checkImageWithNyckel(image)
     formdata.append('file', image);
 
     $.ajax({
-        url:'https://www.nyckel.com/v1/functions/q2jjl438wnztxnel/invoke',
+        url:'https://www.nyckel.com/v1/functions/77gfav5fz7i7f155/invoke',
         type: 'post',
         data: formdata,
         contentType: false,
